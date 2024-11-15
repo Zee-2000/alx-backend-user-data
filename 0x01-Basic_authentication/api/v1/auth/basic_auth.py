2bbbd4739fecfa7b311fda827f8b2335129475cc
Zee-2000 <zahwaabdelnasser@gmail.com>
2024-11-15 22:24:41 +0200

    encoding && decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,3 +15,14 @@ class BasicAuth(Auth):
             return None
         token  = authorization_header.split(" ") [-1]
         return token
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64encode(decoded)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
